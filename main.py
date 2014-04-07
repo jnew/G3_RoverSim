221996789f6d40e17aac02ffcbcd43b6e533e233
John N <jnew93@gmail.com>
2014-04-07 15:29:48 -0400

    prep for ms 4

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #main file for ms3 rover simulation
 import serial
 import argparse
-
+import binascii
 
 class SimCourse:
     """class containing simulation courses"""
@@ -14,21 +14,21 @@ class SimCourse:
         #file object for course data
         with open(filename) as course_file:
             for num, line in enumerate(course_file, 1):
-                as_bytes = bytearray.fromhex(line.partition('#')[0].rstrip())
-                if (num % 2) != 0:
-                    self.total_commands += 1
-                    as_bytes.insert(0, 0x01)
-                    checksum = (as_bytes[1]+as_bytes[2]+as_bytes[3]) & 0x17
-                    as_bytes.insert(4, checksum)
-                    self.sensor_array.append(as_bytes)
-                    print("Read in sensor data:", as_bytes)
-                else:
-                    as_bytes.insert(0, 0xba)
-                    checksum = (as_bytes[1]+as_bytes[2]) & 0x17
-                    as_bytes.append(checksum)
-                    as_bytes.append(0x00)
-                    self.expected_command_array.append(as_bytes)
-                    print("Read in expected move:", as_bytes)
+                if line[0] != '#' and line[0] != ' ':
+                    as_bytes = bytearray.fromhex(line.partition('#')[0].rstrip())
+                    if as_bytes.__len__() == 3:
+                        self.total_commands += 1
+                        as_bytes.insert(0, 0x01)
+                        checksum = (as_bytes[1]+as_bytes[2]+as_bytes[3]) & 0x17
+                        as_bytes.insert(4, checksum)
+                        as_bytes.insert(5, 0x00)
+                        self.sensor_array.append(as_bytes)
+                        print("Read in sensor data:  ", binascii.hexlify(as_bytes))
+                    elif as_bytes.__len__() == 5:
+                        checksum = (as_bytes[1]+as_bytes[2]+as_bytes[3]+as_bytes[4]) & 0x17
+                        as_bytes.append(0x00)
+                        self.expected_command_array.append(as_bytes)
+                        print("Read in expected move:", binascii.hexlify(as_bytes))
 
     def get_sensors(self):
         """returns perfect sensor values"""
@@ -40,14 +40,20 @@ class SimCourse:
         if self.expected_command_array[self.current_place-1] == move_comm:
             return True
         else:
-            print("Incorrect move!\nExpected:", self.expected_command_array[self.current_place-1], "Got:", move_comm)
+            print("Incorrect move!\nExpected:", binascii.hexlify(self.expected_command_array[self.current_place-1]), "Got:", binascii.hexlify(move_comm))
             return False
 
-    def get_movement(self, dist_msg):
+    def get_movement(self):
         """returns perfect movement data"""
-        distance_expected = dist_msg[2]
-        checksum = distance_expected & 0x17
-        return bytes([0x03, distance_expected, checksum, 0x00, 0x00])
+        distance_expected = [0x00, 0x00]
+        if self.expected_command_array[self.current_place-1][0] == 0xBC:
+            distance_expected[0] = 0x3C  # left
+            distance_expected[1] = 0x3C  # right
+        else:
+            distance_expected[0] = self.expected_command_array[self.current_place-1][3]
+            distance_expected[1] = self.expected_command_array[self.current_place-1][4]
+        checksum = distance_expected[0] + distance_expected[1] & 0x17
+        return bytes([0x04, 0x01, distance_expected[0], distance_expected[1], checksum, 0x00])
 
 #time to parse command line args
 parser = argparse.ArgumentParser(description='Simulate G3\'s rover over UART')
@@ -55,29 +61,35 @@ parser.add_argument("port", help="port to open UART connection on")
 parser.add_argument("course_file", help="file to read course from")
 args = parser.parse_args()
 
+#object that will contain course data
+course = SimCourse(args.course_file)
+
 ser = serial.Serial(args.port, 19200, timeout=1)
 if ser.isOpen():
     print("Opened port", args.port)
 
-#object that will contain course data
-course = SimCourse(args.course_file)
+print("\n****SIMULATION BEGIN****")
 
 #loop forever
 while 1:
-    msg = ser.read(5)
-    if msg.__len__() == 5:
-        print("Received:", msg)
+    msg = ser.read(6)
+    if msg.__len__() == 6:
         if msg[0] == 0xAA:  # this is a req for sensor data
+            print("Received:", "Sensor data request", binascii.hexlify(msg))
             reply = course.get_sensors()
             ser.write(reply)
-            print("Replied: ", bytes(reply))
-        elif msg[0] == 0xBA:  # this is a req for movement data
+            print("Replied: ", "Sensor data frame  ", binascii.hexlify(reply))
+        elif msg[0] == 0xBA or msg[0] == 0xBC:  # this is a movement command
+            print("Received:", "Movement command   ", binascii.hexlify(msg))
             if course.check_move_comm(msg):
-                reply = course.get_movement(msg)
+                reply = bytearray([0x03, 0x00, 0x00, 0x00, 0x00, 0x00])
                 ser.write(reply)
-                print("Replied: ", bytes(reply))
+                print("Replied: ", "Movement comm ack  ", binascii.hexlify(reply))
             else:
                 break
+        elif msg[0] == 0xBB:  # this is a request for movement data
+            reply = course.get_movement()
+            ser.write(reply)
             if course.current_place == course.total_commands:
                 print("End of simulation!")
                 exit(0)
